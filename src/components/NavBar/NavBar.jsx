6cf8af850c0fa98c68f04bd576f0437f27ebe98c
oiiemon9 <oiiemon9@gmail.com>
2025-10-09 11:20:19 +0600

    footer complete

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     <div className="bg-base-100">
       <div className="navbar max-w-[1440px] mx-auto px-2">
         <div className="navbar-start">
-          <div className="dropdown">
+          <div className="dropdown mr-2">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -71,8 +71,8 @@ const NavBar = () => {
               </li>
             </ul>
           </div>
-          <Link to="/" className=" text-xl flex items-center gap-1">
-            <img className="w-12" src={Logo} alt="" />
+          <Link to="/" className="text-lg md:text-xl flex items-center gap-1">
+            <img className="w-8 md:w-12" src={Logo} alt="" />
             <p className="font-bold  bg-gradient-to-l from-[#9F62F2]  to-[#632EE3] bg-clip-text text-transparent">
               HERO.IO
             </p>
